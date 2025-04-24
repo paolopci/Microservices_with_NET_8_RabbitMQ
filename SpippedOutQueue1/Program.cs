37f9b5f5546e6b200ad07bff503183db2402d8bb
Paolo Paci <paolopci@yahoo.it>
2025-04-24 17:40:29 +0200

    riprovo invio del messaggio x 3 voltee.UseMessageRetry(r=>     r.Exponential(3,TimeSpan.FromSeconds(1),TimeSpan.FromSeconds(30),TimeSpan.FromSeconds(5)));

--- SpippedOutQueue1/Program.cs
+++ SpippedOutQueue1/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddMassTransit(x =>
         {
 
             // se rimuovo e.Consumer... questo fa si che i messaggi nella coda non vengono consumati
-             e.Consumer<OrderPlacedConsumer>(context);
+            e.Consumer<OrderPlacedConsumer>(context);
 
 
             e.Bind("order-skipped-exchange", e =>
@@ -27,12 +27,21 @@ builder.Services.AddMassTransit(x =>
 
             // provo a spedire il messaggio per 3 volte (con intervallo di 5 secondi tra un tentativo e altro)
             // e poi lo metto in una coda di errore
-            e.UseMessageRetry(r=>r.Interval(3,TimeSpan.FromSeconds(5)));
+            //e.UseMessageRetry(r=>
+            //    r.Exponential(3,TimeSpan.FromSeconds(1),TimeSpan.FromSeconds(30),TimeSpan.FromSeconds(5)));
+
+
+
+            // vedi https://chatgpt.com/c/680b3858-0040-8001-afed-4d92d48c32ce
+            e.UseKillSwitch(options => options
+                .SetActivationThreshold(10)
+                .SetTripThreshold(0.15)
+                .SetRestartTimeout(m: 1));
         });
     });
 });
 
- 
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
