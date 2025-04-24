4915359efc6832d9977cd755f4bc010106b1e08c
Paolo Paci <paolopci@yahoo.it>
2025-04-24 09:22:08 +0200

    add TopicOutQueue1 and TopicOutQueue1

--- TopicService/Program.cs
+++ TopicService/Program.cs
@@ -1,11 +1,41 @@
+using MassTransit;
+using SharedMessages.Messages;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("rabbitmq://localhost");
+        // 5. Configurazione del bus
+        cfg.Message<OrderPlaced>(x => x.SetEntityName("order-placed-exchange"));
+        cfg.Publish<OrderPlaced>(x => x.ExchangeType = "topic");
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapPost("/orders", async (OrderRequest order, IBus bus) =>
+{
+    
+    var orderPlacedMessage = new OrderPlaced(order.orderId, order.quantity);
+    
+    await bus.Publish(orderPlacedMessage, context =>
+    {
+        context.SetRoutingKey(order.quantity > 10 ? "order.shipping" : "order.tracking");
+    });
+
+    return Results.Created($"/orders/{order.orderId}", orderPlacedMessage);
+});
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,29 +46,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+public record OrderRequest(Guid orderId, int quantity);
